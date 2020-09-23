a43630a73cfcaddbbefd1ef29e24ab063b6ae5b9
Felipe Vieira <felipe@codeeasy.com.br>
2020-09-23 19:37:58 -0300

    Veriáveis e constantes

---
+++ 02_variaveis_constantes/README.md
@@ -0,0 +1,5 @@
+Comando para executar o script pelo terminal:
+
+```sh
+php index.php
+```
---
+++ 02_variaveis_constantes/index.php
@@ -0,0 +1,30 @@
+<?php
+
+// Variáveis
+
+$email = readline('Digite o seu e-mail: ');
+$password = readline('Digite a sua senha: ');
+
+echo $email;
+echo "\n";
+
+echo $password;
+echo "\n";
+
+// Constantes
+
+define('SITE_NAME', 'Code Easy');
+
+echo SITE_NAME;
+echo "\n";
+
+// Variáveis Variáveis
+
+$name = 'Felipe';
+$$name = 'Renan';
+
+echo $name;
+echo "\n";
+
+echo $Felipe;
+echo "\n";
