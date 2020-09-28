d7809f4c0fd6d8483bdd02de58d0ece9fb7eb705
Felipe Vieira <felipe@codeeasy.com.br>
2020-09-28 20:50:43 -0300

    Aula 05: Tipos de dados

---
+++ 04_tipos_de_dados/tipos_de_dados.php
@@ -0,0 +1,36 @@
+<?php
+
+// int
+$v1 = 10;
+$v01 = -10;
+$v001 = 0;
+
+// float
+$v2 = 10.5;
+
+// bool
+$v3 = true;
+$v4 = false;
+
+// string
+$v5 = 'string';
+$v6 = "string\n";
+
+// void
+
+// mixed
+// Qualquer tipo
+
+// array
+$v7 = [];
+$v8 = array();
+
+// object
+$v9 = new class
+{
+};
+$v10 = (object) [];
+$v11 = new stdClass();
+
+// gettype
+$type = gettype(10.5);
